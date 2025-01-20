0bb5f1fc497a45105e5f2f8b97fa1db95394bc95
Ankita <bobdeankita24@gmail.com>
2025-01-20 00:45:46 -0330

    registration page complete

---
+++ backend/controllers/userController.js
@@ -0,0 +1,22 @@
+
+const UserModel= require("../models/userModel");
+
+const userRegistration=async(req,res)=>{
+    const {name, email, password} = req.body;
+    const empdata=await UserModel.create({
+        name:name,
+        email:email,
+        password:password
+    })
+
+    // console.log("succesfully registered!");
+    res.send("succesfully registered!");
+}
+
+
+
+
+module.exports={
+    userRegistration
+    
+}
---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app=express();
+var bodyParser = require('body-parser')
+const mongoose = require("mongoose");
+const cors = require('cors');
+const userRoute=require("./routes/userRoutes");
+require("dotenv").config();
+
+mongoose.connect(process.env.DBCONECTION).then(()=>{
+    console.log("DB succesfullly Connected")
+})
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors());
+
+app.use("/users",userRoute); 
+ const port=process.env.PORT || 3000
+
+
+app.listen(port,()=>{
+    console.log(`server run on port ${port}`);
+})
