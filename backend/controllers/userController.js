7452f15de5614b8ca9734349d53760602745fd43
megha thakre <megha1234567891@gmail.com>
2025-01-22 15:50:34 +0530

    insert

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -15,6 +15,25 @@ const userRegistration=async(req,res)=>{
     res.send("succesfully registered!");
 }
 
+const userLogin=async(req, res)=>{
+    const {email, password} = req.body;
+    const empdata= await EmpModel.find({email:email});
+      if (empdata.length<1)
+      {
+        res.status(401).send("Invalid Email!")
+      }
+      else
+      {
+        if (empdata[0].password!=password)
+        {
+            res.status(401).send("Invalid Credentials!");
+        }
+        else
+        {
+            res.status(200).send(empdata);
+        }
+      }
+}
 
 
 module.exports={
